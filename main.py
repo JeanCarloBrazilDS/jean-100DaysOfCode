05d680ae6dc4a7579fdf4e92da5b90fa84cbce79
Jean727 <>
2022-07-14 02:22:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+print ("Hello World")
+print ("something")
+#test 1
+print ('Day - 1 Print Function')
+print ('The Function is declared like this')
+print (" what to print ")
+print (' what to print')
+#single or double clots is equal to computer
+print ("print('what to print')")
+print ("Hello world!\nHello World!\nHello World!")
+#2 ways to write hello Jean
+print ("Hello" +" "+ "Jean")
+print ("hello " + " Jean")
+
+#test 2
+print ("Day 1 - String Manipulation")
+print ('String Concatenation is done with the "+" sign.')
+print ('e.g. print("Hello " + "world")')
+print ("New lines can be created with a backslash and n.")
+
+
+
+
+
+
